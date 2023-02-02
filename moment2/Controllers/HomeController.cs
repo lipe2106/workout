f2ff578da7910defe976c8ba29773cdac36e4117
Lina Petersson <lipe2106@student.miun.se>
2023-02-02 12:46:53 +0100

    Gjort en grund och funktionalitet

---
+++ moment2/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using moment2.Models;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+
+namespace moment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        [Route("/home")]
+        [Route("/index")]
+        [Route("/startsida")]
+        public IActionResult Index()
+        {
+            // Use of ViewBag to send info to index
+            ViewBag.message = "Välkommen till Träningsdagboken! Här kan du hålla kolla på dina avklarade träningspass";
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/")]
+        [Route("/home")]
+        [Route("/index")]
+        [Route("/startsida")]
+        public IActionResult Index(string amount)
+        {
+            // Save input from set goal form in session variable
+            HttpContext.Session.SetString("mySession", amount);
+
+            // Get session variable and show below form
+            ViewBag.sessionContent = "Du har satt ditt mål till " + HttpContext.Session.GetString("mySession") + " gånger. Det kommer du klara!";
+
+            return View();
+        }
+
+
+
+        [HttpGet]
+        [Route("/workout")]
+        [Route("/träning")]
+        public IActionResult Workout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/workout")]
+        [Route("/träning")]
+        public IActionResult Workout(WorkoutModel model)
+        { 
+            if(ModelState.IsValid)
+            {
+                // If input in form is correct
+
+                // Read json- file, convert to list and store according to model
+                var jsonStr = System.IO.File.ReadAllText("workout.json");
+                var jsonObj = JsonConvert.DeserializeObject<List<WorkoutModel>>(jsonStr);
+
+                // Control if json read is correct
+                if(jsonObj != null)
+                {
+                    // Add new workout to list and json- file
+                    jsonObj.Add(model);
+                    System.IO.File.WriteAllText("workout.json", JsonConvert.SerializeObject(jsonObj, Formatting.Indented));
+
+                    // Redirect to result page
+                    return RedirectToAction("Result", "Home");
+                }
+            }
+            return View(); 
+        }
+
+        [HttpGet]
+        [Route("/result")]
+        [Route("/resultat")]
+        public IActionResult Result()
+        {
+            // Get session variable and send to Result view
+            if (HttpContext.Session.GetString("mySession") != null)
+            {
+                ViewBag.sessionContent = "Ditt mål är att träna " + HttpContext.Session.GetString("mySession") + " gånger.";
+            }
+
+            // Read json- file, convert to list and store according to model
+            var jsonStr = System.IO.File.ReadAllText("workout.json");
+            var jsonObj = JsonConvert.DeserializeObject<List<WorkoutModel>>(jsonStr);
+
+            // Return jsonObj to Result view
+            return View(jsonObj);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ moment2/Models/WorkoutModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace moment2.Models
+{
+    public class WorkoutModel
+    {
+        // Properties
+
+        [Required(ErrorMessage = "Du måste ange träningsform")]
+        [Display(Name = "Vilken typ av träning var det?")]
+        public string? Type { get; set; }
+
+        [Required(ErrorMessage = "Du måste ange beskrivning")]
+        [Display(Name = "Beskriv vad du gjort?")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Du måste ange längd")]
+        [Display(Name = "Hur länge tränade du?")]
+        public int? Duration { get; set; }
+
+        [Required(ErrorMessage = "Du måste ange datum")]
+        [Display(Name = "Vilket datum tränade du?")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Du måste ange hur det kändes")]
+        [Display(Name = "Hur kändes träningen?")]
+        public string? Feel { get; set; }
+
+    }
+}
